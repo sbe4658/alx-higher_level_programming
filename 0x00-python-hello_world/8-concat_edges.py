0c5e8c61d923285040cbd405b60ea4e8a0a30179
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-02 15:16:56 +0100

    I give up

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + " with " + str[:6]
+str = "object-oriented programming with Python"
 print(str)
