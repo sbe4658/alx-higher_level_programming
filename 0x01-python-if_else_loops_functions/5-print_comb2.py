294f5f1dbdf03d16c3c0397c531e73c1a8391e54
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-02 22:16:14 +0100

    pad with 0 and print 2 digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(10):
-        if x < 9 or y < 9:
-            print("{:d}{:d}, ".format(x, y), end='')
-        else:
-            print("{:d}{:d}".format(x, y))
+for x in range(100):
+    if x < 99:
+    print("{:02d}, ".format(x, y), end='')
+    else:
+        print("{:d}".format(x))
