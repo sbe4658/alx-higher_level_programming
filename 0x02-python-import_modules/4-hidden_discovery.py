3cd08e9d7f90b1ea469969d464546ba68e9ce212
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-04 22:19:59 +0100

    Nice!

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+    defined = dir(hidden_4)
+    for name in defined:
+        if name[0:2] != "__":
+            print("{:s}".format(name))
