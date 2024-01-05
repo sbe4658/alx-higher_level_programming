20f1ea822761aca6087a400544efc38d28a1a9be
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-06 00:19:24 +0100

    This was this easy?!

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * check_cycle - checks whether a list contains a cycle.
+ * @list: the head node of the list.
+ *
+ * Return: 0 if no cycle was found, if not 1.
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *ahead = list, *last;
+
+	last = ahead;
+	while (ahead)
+	{
+		ahead = ahead->next->next;
+		last = last->next;
+		if (last == ahead)
+			return (1);
+	}
+	return (0);
+}
