33af39d1d66e9e7e6df7d93e71844a745b5d1de5
Alpha 00 <salekbenelhabchi@gmail.com>
2024-01-07 15:09:53 +0100

    well

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    no_c_string = "\0"
+    no_c_string = ""
     for idx in range(0, len(my_string)):
         char = my_string[idx]
         if char not in 'Cc':
