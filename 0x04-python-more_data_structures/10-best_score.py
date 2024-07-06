85d085775b3ff0fef5413957b48400b48b4ff1cc
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-06 17:11:42 +0100

    pycodestyle friendly

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dict):
     tmp = 0
-    if a_dict == None or a_dict == {}:
+    if a_dict is None or a_dict == {}:
         return None
     for key in a_dict:
         if a_dict.get(key) > tmp:
