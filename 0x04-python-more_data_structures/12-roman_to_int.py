58f0d9e2cb0fe90a283c19582e16532e3aeb8c3b
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-06 18:18:24 +0100

    Cheap fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string == "" or not isinstance(roman_string, str):
+    if roman_string is None or roman_string == "":
+        return 0
+    if not isinstance(roman_string, str):
         return 0
     roman_num = {
         'I': 1,
