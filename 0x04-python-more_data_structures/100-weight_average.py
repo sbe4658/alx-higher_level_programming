8ea5938ed046da90ea67aa63293728b030d7d2b5
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-07 00:13:26 +0100

    Is there a better way to do it?

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    x = y = 0
+    for data in my_list:
+        score = data[0]
+        weight = data[1]
+        x += (score * weight)
+        y += weight
+    return x / y
