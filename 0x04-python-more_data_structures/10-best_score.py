7bc00fc5ac7ad3ed964c15db094e3de83e20cc65
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-06 17:10:37 +0100

    confusing

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dict):
+    tmp = 0
+    if a_dict == None or a_dict == {}:
+        return None
+    for key in a_dict:
+        if a_dict.get(key) > tmp:
+            tmp = a_dict.get(key)
+            best_score = key
+    return best_score
