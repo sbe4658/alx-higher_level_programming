4fb8b69fb0dc8e729c6e0220b8db8ee765775a4c
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-07 16:52:37 +0100

    Quick fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,11 +4,8 @@ def list_division(my_list_1, my_list_2, list_length):
     for idx in range(list_length):
         try:
             res.append(my_list_1[idx] / my_list_2[idx])
-        except ValueError:
+        except (ValueError, TypeError):
             print("wrong Type")
-            res.append(0)
-            continue
-        except TypeError:
             res.append(0)
             continue
         except IndexError:
