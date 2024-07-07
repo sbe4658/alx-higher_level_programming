423b3196fd3eb0cfaf4a5bf12098dca6b37a3931
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-07 01:29:06 +0100

    Fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x == 0:
-        print("{}".format(my_list[0]))
-        return 1
+    if x == 0 or my_list = []:
+        return 0
     for idx in range(x):
         try:
             print("{}".format(my_list[idx]), end='')
