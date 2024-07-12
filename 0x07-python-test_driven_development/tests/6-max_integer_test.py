23bdf0af97d049590cae40128948bb9a0753fa4e
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-12 23:03:03 +0100

    Test at the begenning

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,18 +7,30 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """ Edges cases for max_integer([..]). """
-    def empty_list(self):
+    def test_max_infront(self):
+        test = [10, 5, 3]
+        self.assertEqual(max_integer(test), 10)
+
+    def test_max_end(self):
+        test = [10, 5, 13]
+        self.assertEqual(max_integer(test), 13)
+
+    def test_max_middle(self):
+        test = [10, 15, 3]
+        self.assertEqual(max_integer(test), 15)
+
+    def test_empty_list(self):
         self.assertIs(max_integer([]), None)
 
-    def normal(self):
+    def test_normal(self):
         test = [45, 102, 456, 7895]
         self.assertEqual(max_integer(test), 7895)
 
-    def one_element(self):
+    def test_one_element(self):
         test = [0]
         self.assertEqual(max_integer(test), 0)
 
-    def combination(self):
+    def test_combination(self):
         test = [-1, 1.6, 5, 0]
         self.assertEqual(max_integer(test), 5)
 
