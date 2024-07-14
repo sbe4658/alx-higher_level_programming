62f57aeeb79ed23e13e07363eede17f7caf25d41
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-14 23:34:58 +0100

    Rect

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Geometry module. """
+
+
+class BaseGeometry:
+    """ Geometry parent class. """
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError('{:s} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{:s} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ defines a rectangle. """
+    def __init__(self, w, h):
+        self.integer_validator('w', w)
+        self.integer_validator('h', h)
+        self.__height = h
+        self.__width = w
