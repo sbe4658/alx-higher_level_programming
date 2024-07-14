5297283a3eff871508928d55cbee11a7d3cfcff8
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-14 23:38:22 +0100

    OK

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """ Rectangle module. """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """ defines a rectangle. """
     def __init__(self, w, h):
