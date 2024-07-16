dc4cc10292338b839bd43cd05b5f30ac2265e223
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-16 15:58:58 +0100

    Nice

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Defines strundent class. """
+
+
+class Student:
+    """ Student class. """
+    def __init__(self, first_name, last_name, age):
+        """ initialize. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """ Converts to a dictionary. """
+        a = {}
+        if type(attr) is list:
+            for attribute in attr:
+                if hasattr(self, attribute):
+                    a.update({attribute: getattr(self, attribute)})
+        else:
+            a = self.__dict__
+        return a
