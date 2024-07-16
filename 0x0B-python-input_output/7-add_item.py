d151c0c90ea0e600a8f9d820b785dc9abc0b7d2e
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-16 15:06:40 +0100

    Well

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" Write stuff to json file, named add_item.json"""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == '__main__':
+    """ Write stuff to json file, named add_item.json"""
     myFile = 'add_item.json'
     try:
         data = load_from_json_file(myFile)
