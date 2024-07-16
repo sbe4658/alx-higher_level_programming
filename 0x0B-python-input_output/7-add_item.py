596b62daef309563f8fe61d230818447e7abd1b5
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-16 15:04:05 +0100

    Well

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == '__main__':
+    myFile = 'add_item.json'
+    try:
+        data = load_from_json_file(myFile)
+    except Exception:
+        data = []
+    data += sys.argv[1:]
+    save_to_json_file(data, myFile)
