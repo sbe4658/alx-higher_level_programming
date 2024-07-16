8bfc0d15c6d27fc4eb9a72c4d40b310d761ab15d
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-16 15:43:33 +0100

    Kinda got confused, on the last task

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Defines strundent class. """
+
+
+class Student:
+    first_name = 'John'
+    last_name = 'Doe'
+    age = 27
+
+    def __init__(self, first_name, last_name, age):
+        """ initialize. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """ Converts to a dictionary. """
+        return self.__dict__
