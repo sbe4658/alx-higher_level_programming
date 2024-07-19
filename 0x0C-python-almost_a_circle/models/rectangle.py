32823b80c201a88ba0eae0201451a38ef9ec46d1
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-19 19:02:54 +0100

    PLZ

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,15 @@ class Rectangle(Base):
     """ Rectangle objects, defines rect. informations. """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
+        if (type(height) is not int or type(width) is not int
+                or type(x) is not int or type(y) is not int):
+            raise TypeError('width must be an integer')
+        elif width <= 0:
+            raise ValueError('width must be > 0')
+        elif x < 0:
+            raise ValueError('x must be >= 0')
+        elif y < 0:
+            raise ValueError('y must be >= 0')
         self.__width = width
         self.__height = height
         self.__x = x
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,7 +3,8 @@
 from models.rectangle import Rectangle
 import unittest
 
-class  TestRectangle(unittest.TestCase):
+
+class TestRectangle(unittest.TestCase):
     """ Testing Rectangle class. """
     def test_rect_id(self):
         r1 = Rectangle(10, 2)
@@ -12,3 +13,24 @@ class  TestRectangle(unittest.TestCase):
         self.assertEqual(r2.id, 2)
         r3 = Rectangle(2, 10, 0, 0, 12)
         self.assertEqual(r3.id, 12)
+
+    def test_validator(self):
+        Rectangle("12", 13)
+        self.assertRaises(TypeError)
+        r1 = Rectangle(1, 2, 5, 6)
+        r1.width = "Not value"
+        self.assertRaises(TypeError)
+        r1.height = "15"
+        self.assertRaises(TypeError)
+        r1.x = (1, '1')
+        self.assertRaises(TypeError)
+        r1.y = ['a']
+        self.assertRaises(TypeError)
+        r1.x = -1
+        self.assertRaises(ValueError)
+        r1.y = -5
+        self.assertRaises(ValueError)
+        r1.width = -1
+        self.assertRaises(ValueError)
+        r1.height = 0
+        self.assertRaises(ValueError)
