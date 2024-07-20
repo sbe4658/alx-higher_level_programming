8805840349527a124bb49f71d19d406d8817c8fe
Alpha 00 <salekbenelhabchi@gmail.com>
2024-07-20 16:59:26 +0100

    pep8 annoying

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,26 +7,10 @@ class Rectangle(Base):
     """ Rectangle objects, defines rect. informations. """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        if type(width) is not int:
-            raise TypeError('width must be an integer')
-        elif type(height) is not int:
-            raise TypeError('height must be an integer')
-        elif type(x) is not int:
-            raise TypeError('x must be an integer')
-        elif type(y) is not int:
-            raise TypeError('y must be an integer')
-        if width <= 0:
-            raise ValueError('width must be > 0')
-        elif height <= 0:
-            raise ValueError('height must be > 0')
-        elif x < 0:
-            raise ValueError('x must be >= 0')
-        elif y < 0:
-            raise ValueError('y must be >= 0')
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -87,3 +71,8 @@ class Rectangle(Base):
         while h > 0:
             print('#' * w)
             h -= 1
+
+    def __str__(self):
+        return '[Rectangle] ({:}) {}/{} - {}/{}'.format(self.id, self.x,
+                                                        self.y, self.width,
+                                                        self.height)
